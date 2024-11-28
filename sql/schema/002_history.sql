fc8999d05613cc89ca8a4b463de469ae15443d08
hf-chow <honfungchow@gmail.com>
2024-11-29 06:53:29 +0800

    fix syntax error

---
+++ sql/schema/002_history.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE history (
+    id          uuid PRIMARY KEY,
+    user_id     uuid UNIQUE NOT NULL,
+    created_at  timestamp NOT NULL,
+    prompt      text NOT NULL,
+    reply       text NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE history;
