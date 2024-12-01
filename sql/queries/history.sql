55ac725cd5a2c21bb93b1df6d7bdc34d35d4abb4
hf-chow <honfungchow@gmail.com>
2024-12-02 06:39:39 +0800

    fix yaml and generate queries

---
+++ sql/queries/history.sql
@@ -0,0 +1,19 @@
+-- name: CreateHistory :one
+INSERT INTO history (id, user_id, created_at, prompt, reply) 
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5
+)
+RETURNING *;
+
+-- name: GetLastHistoryByUserID :one
+SELECT * FROM history WHERE user_id = $1
+ORDER BY created_at DESC
+LIMIT 1;
+
+-- name: GetAllHistoryByUserID :one
+SELECT * FROM history WHERE user_id = $1
+ORDER BY created_at DESC;
