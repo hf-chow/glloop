38d3b753925cd15e222edfb70954c86f85eb9910
hf-chow <honfungchow@gmail.com>
2024-12-03 07:03:29 +0800

    add read config

--- config.go
+++ config.go
@@ -1,8 +1,48 @@
 package main
 
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
 type Config struct {
 	DBURL				string
 	CurrentUsername		string
 }
 
 const configFilename = ".glloopconfig.json"
+
+func getConfigPath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	path := home + "/" + configFilename
+	return path, nil
+}
+
+func readConfig() (Config, error) {
+	filePath, err := getConfigPath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	jsonFile, err := os.Open(filePath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer jsonFile.Close()
+
+	dat, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return Config{}, err
+	}
+	config := Config{}
+	err = json.Unmarshal(dat, &config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+}
